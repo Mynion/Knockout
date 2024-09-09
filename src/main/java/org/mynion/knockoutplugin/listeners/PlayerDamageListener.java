fe8ebff0f95d0e679985a7b105f892d9b6d614f2
Mynion <s101583@pollub.edu.pl>
2024-09-09 13:22:01 +0200

    Drop when hit
    Added drop KO when carrier hit option

--- src/main/java/org/mynion/knockoutplugin/listeners/PlayerDamageListener.java
+++ src/main/java/org/mynion/knockoutplugin/listeners/PlayerDamageListener.java
@@ -4,6 +4,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.mynion.knockoutplugin.KnockoutPlugin;
 import org.mynion.knockoutplugin.utils.NpcManager;
 
 public class PlayerDamageListener implements Listener {
@@ -25,6 +26,13 @@ public class PlayerDamageListener implements Listener {
                     e.setCancelled(true);
                     NpcManager.knockoutPlayer(p);
                 }
+            } else if (KnockoutPlugin.getPlugin().getConfig().getBoolean("drop-when-hit")) {
+                // Drop knocked out player when hit
+                p.getPassengers().stream()
+                        .filter(passenger -> passenger instanceof Player)
+                        .map(passenger -> (Player) passenger)
+                        .filter(NpcManager::npcExists)
+                        .forEach(knockedOutPlayer -> NpcManager.stopCarrying(knockedOutPlayer, p));
             }
         }
     }
