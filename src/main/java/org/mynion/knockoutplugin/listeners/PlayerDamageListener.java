32c1e4cb95680f6aafbd5b0cc2b35b4ceb12f82d
Mynion <s101583@pollub.edu.pl>
2024-10-24 17:22:22 +0200

    Totem of Undying fix
    Totems didnt work

--- src/main/java/org/mynion/knockoutplugin/listeners/PlayerDamageListener.java
+++ src/main/java/org/mynion/knockoutplugin/listeners/PlayerDamageListener.java
@@ -1,5 +1,6 @@
 package org.mynion.knockoutplugin.listeners;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,8 +14,7 @@ public class PlayerDamageListener implements Listener {
         if (e.getEntity() instanceof Player p) {
 
             // Check if the player would die
-            if (e.getFinalDamage() >= p.getHealth() && !hasDamageCooldown(p)) {
-
+            if (e.getFinalDamage() >= p.getHealth() && !hasDamageCooldown(p) && !hasTotemOfUndying(p)) {
                 // Check if the player is knocked out
                 if (NpcManager.npcExists(p)) {
 
@@ -40,5 +40,11 @@ public class PlayerDamageListener implements Listener {
     // Check if the player has damage cooldown to prevent false event calls
     private boolean hasDamageCooldown(Player p) {
         return p.getNoDamageTicks() > 10;
+    }
+
+    // Check if the player has totem of undying in either of his hands
+    private boolean hasTotemOfUndying(Player p){
+        return p.getInventory().getItemInMainHand().getType().equals(Material.TOTEM_OF_UNDYING)
+                || p.getInventory().getItemInOffHand().getType().equals(Material.TOTEM_OF_UNDYING);
     }
 }
