704b4f8ade4f7e00d3eed3067891a4f8c76b276c
Mynion <s101583@pollub.edu.pl>
2025-01-25 01:05:59 +0100

    mobs have a focus on ko players and dont attack others fix

--- common/src/main/java/org/mynion/knockoutplugin/listeners/cancelled/EntityTargetListener.java
+++ common/src/main/java/org/mynion/knockoutplugin/listeners/cancelled/EntityTargetListener.java
@@ -1,11 +1,16 @@
 package org.mynion.knockoutplugin.listeners.cancelled;
 
+import org.bukkit.GameMode;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityTargetEvent;
 import org.mynion.knockoutplugin.Knockout;
 import org.mynion.knockoutplugin.utils.NpcManager;
+
+import java.util.List;
+import java.util.Optional;
 
 public class EntityTargetListener implements Listener {
     @EventHandler
@@ -14,6 +19,12 @@ public class EntityTargetListener implements Listener {
             NpcManager NpcManager = Knockout.getNpcManager();
             if (NpcManager.npcExists(p)) {
                 e.setCancelled(true);
+                Optional<Player> newTarget = p.getNearbyEntities(10, 5, 10).stream()
+                        .filter(entity -> entity instanceof Player)
+                        .map(entity -> (Player) entity)
+                        .filter(player -> List.of(GameMode.ADVENTURE, GameMode.SURVIVAL).contains(player.getGameMode()))
+                        .findFirst();
+                newTarget.ifPresent(e::setTarget);
             }
         }
     }
