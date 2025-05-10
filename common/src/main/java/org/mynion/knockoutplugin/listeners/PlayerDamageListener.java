9c6b26ed5fbcaf7ae580136ba5404c6988f25911
Mynion <s101583@pollub.edu.pl>
2025-05-10 16:59:50 +0200

    recursive damageEvent fix

--- common/src/main/java/org/mynion/knockoutplugin/listeners/PlayerDamageListener.java
+++ common/src/main/java/org/mynion/knockoutplugin/listeners/PlayerDamageListener.java
@@ -36,7 +36,9 @@ public class PlayerDamageListener implements Listener {
 
                             // Damage by attacker
                             NpcManager.getNpc(p).setVulnerableByPlayerWhenCarried(true);
-                            p.damage(e.getFinalDamage(), NpcManager.getKiller(p));
+                            Entity killer = NpcManager.getKiller(p);
+                            NpcManager.endKnockout(p, false);
+                            p.damage(e.getFinalDamage(), killer);
 
                             // p.damage() calls EntityDamageEvent, so we return to prevent ending knockout two times
                             return;
